70cac57e81adbc4436409f64554021ccead351be
Mark Shenouda <mark@thisdot.co>
2022-02-28 13:39:48 +0200

    feat: setup next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Simple Usage Example",
+  description: "A short description goes here.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://chainlink-developer-hub.vercel.app/",
+    site_name: "ChinLink Developer Hub",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
