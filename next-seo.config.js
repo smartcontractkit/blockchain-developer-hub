1c0870e59b80e74a35e2f160eae0e2f7f32ab1ff
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-01 14:40:56 +0200

    [feat] Setting up the Static Analysis Testing Tools (#21)
    * feat: add more eslint rules
    
    * feat: add husky hooks
    
    * feat: add the lint staged
    
    * fix: update prettier
    
    * fix: install husky@4
    
    * fix: installed husky@3
    
    * fix: reinstall husky
    
    * fix: reinstall husky
    
    * fix: test husky
    
    * fix: test husky
    
    * fix: remove lintstaged
    
    * fix: cleanup
    
    * fix: format
    
    * fix: lint
    
    Co-authored-by: Mark Shenouda <mark@thisdot.co>

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,15 +1,15 @@
 export default {
-  title: "ChainLink Developer Hub",
-  description: "A short description goes here.",
+  title: 'ChainLink Developer Hub',
+  description: 'A short description goes here.',
   openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://chainlink-developer-hub.vercel.app/",
-    site_name: "ChinLink Developer Hub",
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://chainlink-developer-hub.vercel.app/',
+    site_name: 'ChinLink Developer Hub',
   },
   twitter: {
-    handle: "@handle",
-    site: "@site",
-    cardType: "summary_large_image",
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
   },
 };
