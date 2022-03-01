6989804c4e98a131c355279f63805c121e096424
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-01 09:58:41 +0200

    feat: setup next-seo (#10)
    * feat: setup next-seo
    
    * fix: add title
    
    Co-authored-by: Mark Shenouda <mark@thisdot.co>

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+  title: "ChainLink Developer Hub",
+  description: "A short description goes here.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://chainlink-developer-hub.vercel.app/",
+    site_name: "ChinLink Developer Hub",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
