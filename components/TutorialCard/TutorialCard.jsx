5824315b53a609207b3fddf183129607dc77001b
Jerry Hogan <jerry@enyata.com>
2022-03-03 15:24:42 +0100

    Tutorial card completed

--- components/TutorialCard/TutorialCard.jsx
+++ components/TutorialCard/TutorialCard.jsx
@@ -4,12 +4,13 @@ import styles from './TutorialCard.module.css';
 
 export default function TutorialCard({ heading, author, date, description, variant }) {
   const classes = clsx(styles.tut_card, { [styles.tut_card_gray]: variant === 'gray' });
-  const author_date_classes = clsx(styles.tut_card_content, styles.author_date);
+  const heading_classes = clsx(styles.tut_card_content, styles.tur_card_heading);
+  const author_date_classes = clsx(styles.tut_card_content, styles.tur_card_author_date);
   const description_classes = clsx(styles.tut_card_content, styles.tut_card_description);
 
   return (
     <div className={classes}>
-      <h3>{heading}</h3>
+      <h3 className={heading_classes}>{heading}</h3>
       <p className={author_date_classes}>
         <small>{author}</small>
         <span>|</span>
