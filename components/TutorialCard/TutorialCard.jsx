1c0bbe4fbb0fc46a365d99208730a2daba91ebe1
Jerry Hogan <jerry@enyata.com>
2022-03-03 15:18:12 +0100

    Tutorial card completed

--- components/TutorialCard/TutorialCard.jsx
+++ components/TutorialCard/TutorialCard.jsx
@@ -1,10 +1,33 @@
-// import styles from './TutorialCard.module.css';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import styles from './TutorialCard.module.css';
 
-export default function TutorialCard() {
-  return <></>;
+export default function TutorialCard({ heading, author, date, description, variant }) {
+  const classes = clsx(styles.tut_card, { [styles.tut_card_gray]: variant === 'gray' });
+  const author_date_classes = clsx(styles.tut_card_content, styles.author_date);
+  const description_classes = clsx(styles.tut_card_content, styles.tut_card_description);
+
+  return (
+    <div className={classes}>
+      <h3>{heading}</h3>
+      <p className={author_date_classes}>
+        <small>{author}</small>
+        <span>|</span>
+        <small>{date}</small>
+      </p>
+      {description && <p className={description_classes}>{description}</p>}
+    </div>
+  );
 }
 
 TutorialCard.propTypes = {
   heading: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  variant: PropTypes.oneOf(['default', 'gray']),
+};
+
+TutorialCard.defaultProps = {
+  variant: 'default',
 };
