e585cdb14a83fc191407559f6e2a1eebca7a39be
Mark Shenouda <mark@thisdot.co>
2022-03-03 14:50:51 +0200

    fix: use Svg and css variables

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -2,6 +2,7 @@ import styles from './Card.module.css';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import clsx from 'clsx';
+import Svg from '@/components/Svg';
 
 function Card({ variant = 'gray', title, description, image, href, online, location, date, prize }) {
   const classes = clsx(styles.container, { [styles.light]: variant === 'light' });
@@ -15,7 +16,7 @@ function Card({ variant = 'gray', title, description, image, href, online, locat
 
         {prize && (
           <div className={styles.prize}>
-            <object type="image/svg+xml" height="18" width="18" data="/icons/prize.svg" />
+            <Svg height="18" width="18" href="/icons/prize.svg" />
             {prize} in prizes
           </div>
         )}
@@ -25,9 +26,9 @@ function Card({ variant = 'gray', title, description, image, href, online, locat
           {(online || location) && (
             <span>
               {online ? (
-                <object type="image/svg+xml" height="20" width="20" data="/icons/online.svg" />
+                <Svg height="20" width="20" href="/icons/online.svg" />
               ) : (
-                <object type="image/svg+xml" height="20" width="20" data="/icons/location.svg" />
+                <Svg height="20" width="20" href="/icons/location.svg" />
               )}
               {online ? 'Online' : location}
             </span>
@@ -36,7 +37,7 @@ function Card({ variant = 'gray', title, description, image, href, online, locat
           {/* If date exist */}
           {date && (
             <span>
-              <object type="image/svg+xml" height="20" width="20" data="/icons/calender.svg" />
+              <Svg height="20" width="20" href="/icons/calender.svg" />
               {date}
             </span>
           )}
