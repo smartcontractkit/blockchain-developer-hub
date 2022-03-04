93743d5d4b56d9586e0d07d1d7d87e898332ce82
Jerry Hogan <jerry@enyata.com>
2022-03-04 11:44:48 +0100

    Ecosystem page under dev

--- pages/ecosystem-map/EcosystemMap.jsx
+++ pages/ecosystem-map/EcosystemMap.jsx
@@ -1,4 +1,5 @@
 import NavBar from '@/components/NavBar';
+import NavLink from '@/components/NavLink';
 import styles from './EcosystemMap.module.css';
 
 function EcosystemMap() {
@@ -6,7 +7,14 @@ function EcosystemMap() {
     <div>
       <NavBar />
       <main className={styles.container}>
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          <h1>The Smart Contract Ecosystem Map</h1>
+          <p>
+            The ecosystem tools map outlines the entire smart contract developer ecosystem, and all of the tools and
+            products smart contract developers may need to rely on.
+          </p>
+          <NavLink type="primary" />
+        </div>
       </main>
     </div>
   );
