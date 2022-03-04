fdf1acf78893dfcd6f66e943ecef50f1f7077881
Jerry Hogan <jerry@enyata.com>
2022-03-04 14:54:56 +0100

    Fix: made index.js file where the page will be

--- pages/ecosystem-map/index.js
+++ pages/ecosystem-map/index.js
@@ -1,1 +1,27 @@
-export { default as default } from './EcosystemMap';
+import NavLink from '@/components/NavLink';
+import styles from './EcosystemMap.module.css';
+
+function EcosystemMap() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1>The Smart Contract Ecosystem Map</h1>
+        <p>
+          The ecosystem tools map outlines the entire smart contract developer ecosystem, and all of the tools and
+          products smart contract developers may need to rely on.
+        </p>
+        <NavLink
+          to="https://smartcontractecosystem.com/"
+          type="primary"
+          text="Explore Map"
+          iconPosition="right"
+          icon="map-pointer.svg"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default EcosystemMap;
