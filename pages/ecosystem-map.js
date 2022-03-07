c775cdf26ac37f18a68e02912990907eccaed1e9
Jerry Hogan <damilolajerryhd@gmail.com>
2022-03-07 12:11:35 +0100

    Feat: Ecosystem map page (#47)
    * Feat: Base Link for the project
    
    * Feat: Restructured Nav Link for better use
    
    * Feat: PropTypes for NavLink component, storybook
    
    * merged main branch
    
    * Fix: Made some adjustment to the component, remove duplicate styles
    
    * Removed comment, useRouter,  and exact props in NavLink
    
    * Lint format
    
    * Fix: code review corrections
    
    * Fix: code review corrections
    
    * Fix: code review corrections
    
    * Fix: code review corrections
    
    * file rename, navbar component in progress with storybook
    
    * Feat: Nav Bar component design, reponsiveness, storybook
    
    * Feat: Nav Bar component design, reponsiveness, storybook
    
    * H1 for logo
    
    * code review, changes and restructuring
    
    * Made links wrap to new row
    
    * Merged main branch
    
    * Merged main branch, replace old values
    
    * revert icon changes
    
    * revert icon changes
    
    * Tutorial Card in progress
    
    * Resolved path issue for storybook
    
    * Tutorial card completed
    
    * Tutorial card completed
    
    * Added root color variable
    
    * Fix: Made some correections with margins
    
    * Feat: Cursor pointer for card
    
    * Updated index.js
    
    * Ecosystem page under dev
    
    * Ecosystem page under dev
    
    * Feat: Ecosystem page completed, explore map link opens new tab
    
    * Feat: Ecosystem page style cleanups
    
    * Feat: Ecosystem page style cleanups
    
    * Fix: made index.js file where the page will be
    
    * Fix: file renamed, style added to pages folder in styles folder
    
    Co-authored-by: Jerry Hogan <jerry@enyata.com>

--- pages/ecosystem-map.js
+++ pages/ecosystem-map.js
@@ -1,5 +1,27 @@
+import NavLink from '@/components/NavLink';
+import styles from '@/styles/pages/EcosystemMap.module.css';
+
 function EcosystemMap() {
-  return <h1>Ecosystem Map</h1>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1>The Smart Contract Ecosystem Map</h1>
+        <p>
+          The ecosystem tools map outlines the entire smart contract developer ecosystem, and all of the tools and
+          products smart contract developers may need to rely on.
+        </p>
+        <NavLink
+          to="https://smartcontractecosystem.com/"
+          type="primary"
+          text="Explore Map"
+          iconPosition="right"
+          icon="map-pointer.svg"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default EcosystemMap;
