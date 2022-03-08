883590a7d9c95ecf3e9b26829b71ec0589ca2b37
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-08 16:02:42 +0100

    Updated learn hero section classes with the typography classes

--- sections/LearnHero/LearnHero.jsx
+++ sections/LearnHero/LearnHero.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './LearnHero.module.css';
 import Svg from '@/components/Svg';
+import clsx from 'clsx';
 
 const BLOCKCHAINS = [
   {
@@ -21,21 +22,26 @@ const BLOCKCHAINS = [
 ];
 
 function LearnHero() {
+  const titleClasses = clsx('h-500', styles.title);
+  const descriptionClasses = clsx('body-long-01', styles.description);
+  const dividerClasses = clsx('caption', styles.divider);
+  const linkClasses = clsx('btn', styles.link);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Learn how to code smart contracts.</h1>
+      <h1 className={titleClasses}>Learn how to code smart contracts.</h1>
 
-      <p className={styles.description}>
+      <p className={descriptionClasses}>
         Smart contracts are digital contracts stored on a blockchain that are automatically executed when predetermined
         terms and conditions are met
       </p>
 
-      <div className={styles.divider}>Select a blockchain to get started</div>
+      <div className={dividerClasses}>Select a blockchain to get started</div>
       <div className={styles.blockchainsContainer}>
         {BLOCKCHAINS.map(({ name, image, href }, index) => (
           <div className={styles.blockchainCard} key={index}>
             <Image src={image} alt={name} width={100} height={100} objectFit="contain" />
-            <a href={href} className={styles.link}>
+            <a href={href} className={linkClasses}>
               {name}
               <Svg height="20" width="20" href="/icons/arrow-right-blue.svg" />
             </a>
