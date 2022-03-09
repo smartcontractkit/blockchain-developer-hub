21c3f1f22d2847480018245985f9d300684cecfa
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-09 18:41:14 +0200

    Pr 38 (#59)
    * feat: Blockchain101
    
    * fix: markdown content

--- pages/blockchain101/blockchain.js
+++ pages/blockchain101/blockchain.js
@@ -2,24 +2,35 @@ import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import getPage from '@/helpers/getPage';
 import PropTypes from 'prop-types';
+import BlogLayout from '@/layouts/BlogLayout';
+import ArticleHeader from '@/components/ArticleHeader';
+import mdxStyles from '@/styles/MDX.module.css';
 
-function Blockchain({ content }) {
+function Blockchain({ data, content }) {
   return (
-    <div>
-      <h1>Blockchain 101 - Blockchain</h1>
-
-      <MDXRemote {...content} />
-    </div>
+    <BlogLayout>
+      <ArticleHeader
+        title={data.title}
+        author={data.author}
+        role={data.author_role}
+        image={data.author_image}
+        datetime={data.datetime}
+      />
+      <div className={mdxStyles.container}>
+        <MDXRemote {...content} />
+      </div>
+    </BlogLayout>
   );
 }
 
 Blockchain.propTypes = {
+  data: PropTypes.object.isRequired,
   content: PropTypes.object.isRequired,
 };
 export default Blockchain;
 
 export const getStaticProps = async () => {
-  const page = await getPage('blockchain101/blockchain.mdx');
+  const page = await getPage('blockchain101/blockchain.md');
   const mdxSource = await serialize(page.content);
   return {
     props: {
