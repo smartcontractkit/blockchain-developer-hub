47d66dab22618d5ed146e5ad3638381b0db40c9e
Mark Shenouda <mark@thisdot.co>
2022-03-09 15:55:48 +0200

    feat: Blog Layout

--- pages/ship.js
+++ pages/ship.js
@@ -2,14 +2,22 @@ import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import getPage from '@/helpers/getPage';
 import PropTypes from 'prop-types';
+import BlogLayout from '@/layouts/BlogLayout';
+import ArticleHeader from '@/components/ArticleHeader';
 
 function Ship({ content }) {
   return (
-    <div>
-      <h1>Ship page with just MD</h1>
+    <BlogLayout>
+      <ArticleHeader
+        title="How to ship your dApp"
+        author="James Bond"
+        role="Software Engineer at Chainlink"
+        image="/logos/solana.png"
+        datetime="2020-01-01"
+      />
 
       <MDXRemote {...content} />
-    </div>
+    </BlogLayout>
   );
 }
 
