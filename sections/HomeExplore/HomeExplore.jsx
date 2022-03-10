6bc050707a6dc6cbba830971f613c30317b53ef7
Mark Shenouda <mark@thisdot.co>
2022-03-10 14:51:05 +0200

    feat: Home Explore section

---
+++ sections/HomeExplore/HomeExplore.jsx
@@ -0,0 +1,13 @@
+import styles from './HomeExplore.module.css';
+
+function HomeExplore() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.innerContainer}>
+        <h1>Hi</h1>
+      </div>
+    </section>
+  );
+}
+
+export default HomeExplore;
