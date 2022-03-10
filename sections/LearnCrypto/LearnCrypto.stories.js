0c6514bed99dcb6957404e5e6c5d0ea5f8f437ac
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-10 15:13:39 +0100

    Updates: Yaml file used to test now

--- sections/LearnCrypto/LearnCrypto.stories.js
+++ sections/LearnCrypto/LearnCrypto.stories.js
@@ -1,69 +1,7 @@
 import LearnCrypto from './LearnCrypto';
-// These data courses and tutorials can be removed when we seolve the yaml error so it can be pull from there to test.
-const courses = {
-  overview:
-    'Competing offers opportunities to learn blockchain development online and gives you the potential to win amazing prizes through both in-person and online virtual hackathons.',
-  data: [
-    {
-      title: 'XRPL Hackathon: New Year, New NFTs',
-      author: 'Patrick Collins',
-      image: '/images/learn.jpg',
-      level: 'Intermediate',
-      description:
-        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
-    },
-    {
-      title: 'Complete DApp - Solidity & React - Blockchain Development',
-      author: 'Patrick Collins',
-      image: '/images/learn.jpg',
-      level: 'Intermediate',
-      description:
-        'Master DApp Development - Smart Contracts - Solidity, Web3, React, Mocha, Ethereum Blockchain Programming for Front-end',
-    },
-    {
-      title: 'Ethereum Blockchain Developer Bootcamp With Solidity (2022)',
-      author: 'Patrick Collins',
-      image: '/images/learn.jpg',
-      level: 'Intermediate',
-      description:
-        'Become An Ethereum Blockchain Developer With One Course. Master Solidity, Web3.JS, Truffle, Metamask, Remix & More!',
-    },
-  ],
-};
-const tutorials = {
-  overview:
-    'Competing offers opportunities to learn blockchain development online and gives you the potential to win amazing prizes through both in-person and online virtual hackathons.',
-  data: [
-    {
-      title: 'XRPL Hackathon: New Year, New NFTs',
-      author: 'Patrick Collins',
-      data: '022-02-12',
-      description:
-        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
-    },
-    {
-      title: 'Complete DApp - Solidity & React - Blockchain Development',
-      author: 'Patrick Collins',
-      data: '022-02-12',
-      description:
-        'Master DApp Development - Smart Contracts - Solidity, Web3, React, Mocha, Ethereum Blockchain Programming for Front-end',
-    },
-    {
-      title: 'Ethereum Blockchain Developer Bootcamp With Solidity (2022)',
-      author: 'Patrick Collins',
-      data: '022-02-12',
-      description:
-        'Become An Ethereum Blockchain Developer With One Course. Master Solidity, Web3.JS, Truffle, Metamask, Remix & More!',
-    },
-    {
-      title: 'XRPL Hackathon: New Year, New NFTs',
-      author: 'Patrick Collins',
-      data: '022-02-12',
-      description:
-        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
-    },
-  ],
-};
+import data from '@/data/learn.yaml';
+const { resources } = data;
+const { id, name, logo, courses, tutorials } = resources[0];
 
 export default {
   title: 'section/Learn Crypto',
@@ -75,12 +13,12 @@ const Template = (args) => <LearnCrypto {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  id: 'evm-chains',
-  name: 'EVM Chains',
-  logo: '/logos/ethereum.png',
-  logoAlt: 'EVM Chains Logo',
-  courses: courses,
-  tutorials: tutorials,
+  id,
+  name,
+  logo,
+  logoAlt: `${name} Logo`,
+  courses,
+  tutorials,
 };
 
 Default.parameters = {
