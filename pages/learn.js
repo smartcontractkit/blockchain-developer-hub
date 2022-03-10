f19ae0d39b7cae35e65869d24a6466adb8a3a938
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-10 13:05:07 +0100

    Added dummy courses and tutorials pending yaml file issue fixed for storybook

--- pages/learn.js
+++ pages/learn.js
@@ -1,26 +1,9 @@
-import LearnCrypto from '@/sections/LearnCrypto';
 import LearnHero from '@/sections/LearnHero';
-import learnData from '@/data/learn.yaml';
 
 function Learn() {
-  const { resources } = learnData;
-  // const { id, name, logo, courses, tutorials } = resources[0];
-
   return (
     <div>
       <LearnHero />
-      {resources.map(({ id, name, logo, courses, tutorials }) => (
-        <LearnCrypto
-          key={id}
-          id={id}
-          name={name}
-          logo={logo}
-          logoAlt={name + ' Logo'}
-          courses={courses}
-          tutorials={tutorials}
-        />
-      ))}
-      {/* <LearnCrypto id={id} name={name} logo={logo} logoAlt={name + ' Logo'} courses={courses} tutorials={tutorials} /> */}
     </div>
   );
 }
--- sections/LearnCrypto/LearnCrypto.stories.js
+++ sections/LearnCrypto/LearnCrypto.stories.js
@@ -1,8 +1,68 @@
 import LearnCrypto from './LearnCrypto';
-import dummy from '@/data/learn.yaml';
-console.log(dummy);
-// import dummy from '@/data/dummy.json';
-// console.log(dummy);
+const courses = {
+  overview:
+    'Competing offers opportunities to learn blockchain development online and gives you the potential to win amazing prizes through both in-person and online virtual hackathons.',
+  data: [
+    {
+      title: 'XRPL Hackathon: New Year, New NFTs',
+      author: 'Patrick Collins',
+      image: '/images/learn.jpg',
+      level: 'Intermediate',
+      description:
+        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
+    },
+    {
+      title: 'Complete DApp - Solidity & React - Blockchain Development',
+      author: 'Patrick Collins',
+      image: '/images/learn.jpg',
+      level: 'Intermediate',
+      description:
+        'Master DApp Development - Smart Contracts - Solidity, Web3, React, Mocha, Ethereum Blockchain Programming for Front-end',
+    },
+    {
+      title: 'Ethereum Blockchain Developer Bootcamp With Solidity (2022)',
+      author: 'Patrick Collins',
+      image: '/images/learn.jpg',
+      level: 'Intermediate',
+      description:
+        'Become An Ethereum Blockchain Developer With One Course. Master Solidity, Web3.JS, Truffle, Metamask, Remix & More!',
+    },
+  ],
+};
+const tutorials = {
+  overview:
+    'Competing offers opportunities to learn blockchain development online and gives you the potential to win amazing prizes through both in-person and online virtual hackathons.',
+  data: [
+    {
+      title: 'XRPL Hackathon: New Year, New NFTs',
+      author: 'Patrick Collins',
+      data: '022-02-12',
+      description:
+        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
+    },
+    {
+      title: 'Complete DApp - Solidity & React - Blockchain Development',
+      author: 'Patrick Collins',
+      data: '022-02-12',
+      description:
+        'Master DApp Development - Smart Contracts - Solidity, Web3, React, Mocha, Ethereum Blockchain Programming for Front-end',
+    },
+    {
+      title: 'Ethereum Blockchain Developer Bootcamp With Solidity (2022)',
+      author: 'Patrick Collins',
+      data: '022-02-12',
+      description:
+        'Become An Ethereum Blockchain Developer With One Course. Master Solidity, Web3.JS, Truffle, Metamask, Remix & More!',
+    },
+    {
+      title: 'XRPL Hackathon: New Year, New NFTs',
+      author: 'Patrick Collins',
+      data: '022-02-12',
+      description:
+        'Build an app for open source collaboration on maps of problems through crowdsourcing and crowdfunding.',
+    },
+  ],
+};
 
 export default {
   title: 'section/Learn Crypto',
@@ -18,13 +78,10 @@ Default.args = {
   name: 'EVM Chains',
   logo: '/logos/ethereum.png',
   logoAlt: 'EVM Chains Logo',
-  courses: {},
-  tutorials: {},
+  courses: courses,
+  tutorials: tutorials,
 };
 
 Default.parameters = {
-  backgrounds: {
-    default: 'gray',
-  },
   layout: 'fullscreen',
 };
