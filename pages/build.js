e62fc0fe7770d9134ea791d913119e2f6d25c491
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-11 11:07:49 +0100

    Feat: Build Page implementation, section component created for re-useability (#71)
    * Feat: Build Page implementation, section component created for re-useability
    
    * Fix: Moved test images to demo folder

--- pages/build.js
+++ pages/build.js
@@ -1,5 +1,15 @@
+import data from '@/data/build.yaml';
+import BuildPageSection from '@/sections/BuildPageSection';
+import styles from '@/styles/pages/BuildPage.module.css';
 function Build() {
-  return <h1>Build</h1>;
+  const { categories } = data;
+  return (
+    <div className={styles.container}>
+      {categories.map(({ name, overview, items }, index) => (
+        <BuildPageSection key={index} name={name} overview={overview} items={items} />
+      ))}
+    </div>
+  );
 }
 
 export default Build;
