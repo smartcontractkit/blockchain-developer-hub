e08a00bf423f80b124b7c5a21a928f3c68de28a7
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-11 11:26:22 +0100

    Feat: Learn page including responsive design

--- pages/learn.js
+++ pages/learn.js
@@ -1,9 +1,25 @@
 import LearnHero from '@/sections/LearnHero';
+import data from '@/data/learn.yaml';
+import LearnCrypto from '@/sections/LearnCrypto';
 
 function Learn() {
+  const { resources } = data;
   return (
     <div>
       <LearnHero />
+      <section>
+        {resources.map(({ id, name, logo, courses, tutorials }) => (
+          <LearnCrypto
+            key={id}
+            id={id}
+            name={name}
+            logo={logo}
+            logoAlt={`${name} Logo`}
+            courses={courses}
+            tutorials={tutorials}
+          />
+        ))}
+      </section>
     </div>
   );
 }
