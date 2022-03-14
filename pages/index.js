54aabe709c5273d3c3a13eed31c8d4b3de42de41
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-14 16:30:25 +0100

    Home page in progress

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import HomePageBanner from '@/sections/HomePageBanner';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <HomePageBanner
+        title="Smart Contracts Development"
+        overview="Covering the fundamentals of web3 development: from writing Solidity, to minting NFTs, to building full-stack dApps. Bringing you resources from the best in blockchain."
+        label="Everything about"
+        link="/"
+        linkText="Quickstart"
+        image="https://www.fillmurray.com/640/360"
+        imageAlt="Banner image"
+      />
+    </div>
+  );
 }
