8d5070e17a9aae89a7710ea33148eb32aa93c8e1
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-16 11:41:24 +0200

    feat: Home Page Improve Skills Section (#78)
    * feat: Home Page Improve Skills Section
    
    * fix: grid width

---
+++ sections/HomePageImproveSkills/HomePageImproveSkills.jsx
@@ -0,0 +1,23 @@
+import CardWithArrow from '@/components/CardWithArrow';
+import buildData from '@/data/build.yaml';
+import styles from './HomePageImproveSkills.module.css';
+import clsx from 'clsx';
+
+function HomePageImproveSkills() {
+  console.log(buildData);
+  return (
+    <section className={styles.container}>
+      <div>
+        <h2 className={clsx('h-400', styles.title)}>Improve your skills on the field</h2>
+        <p className={clsx('body-short-2', styles.description)}>Want to experiment first, ask questions later?</p>
+      </div>
+      <div className={styles.categories}>
+        {buildData.categories.map((category, index) => (
+          <CardWithArrow key={index} title={category.name} description={category.overview} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default HomePageImproveSkills;
