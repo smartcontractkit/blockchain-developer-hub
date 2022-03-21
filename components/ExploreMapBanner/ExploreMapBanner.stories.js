1b947d57f088b2cee4a1151b5ed743c2c46ef9de
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-21 16:30:46 +0100

    Updates

---
+++ components/ExploreMapBanner/ExploreMapBanner.stories.js
@@ -0,0 +1,10 @@
+import ExploreMapBanner from './ExploreMapBanner';
+
+export default {
+  title: 'title/Explore Map Banner',
+  component: ExploreMapBanner,
+};
+
+const Template = (args) => <ExploreMapBanner {...args} />;
+
+export const Default = Template.bind({});
