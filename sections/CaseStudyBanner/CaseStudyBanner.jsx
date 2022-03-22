1227dbf1b1dfc6fffbad35ea834c53b18fe169cd
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-22 17:10:51 +0100

    Feat: Implementation of case studies banner for the home page

---
+++ sections/CaseStudyBanner/CaseStudyBanner.jsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+import NavLink from '@/components/NavLink';
+import styles from './CaseStudyBanner.module.css';
+
+export default function CaseStudyBanner() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content_wrapper}>
+        <div className={styles.image}>
+          <Image src={'/demo/casestudy.png'} alt={'Case study Banner image'} layout="fill" objectFit="cover" />
+        </div>
+        <div className={styles.info}>
+          <h3 className={clsx('h-300', styles.title)}>Check how Horizon Games utilizing power of blockchain</h3>
+          <p className={clsx('body-long-01', styles.overview)}>
+            Horizon Games partnered with Infura to battle-test SkyWeaver’s gaming system in order to ensure robust
+            functionality and facilitate widespread adoption.
+          </p>
+          <div className={styles.actions}>
+            <span>
+              <NavLink to={'#'} target="_blank" rel="noopener noreferrer" type="primary" text={'Learn more'} />
+            </span>
+            <span>
+              <NavLink
+                to={'#'}
+                target="_blank"
+                rel="noopener noreferrer"
+                type="secondary"
+                text={'All case studies'}
+                iconPosition="left"
+                icon="union.svg"
+              />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ sections/CaseStudyBanner/CaseStudyBanner.stories.js
@@ -0,0 +1,10 @@
+import CaseStudyBanner from './CaseStudyBanner';
+
+export default {
+  title: 'section/HomePage Banner',
+  component: CaseStudyBanner,
+};
+
+const Template = (args) => <CaseStudyBanner {...args} />;
+
+export const Default = Template.bind({});
