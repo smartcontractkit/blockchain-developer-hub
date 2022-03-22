eec13c11c060dc72dee2a933d96f5b3815af50c6
Jerry Hogan <jerry.hogan@thisdot.co>
2022-03-22 10:48:01 +0100

    Fix: added fixes

--- sections/LearnHero/LearnHero.jsx
+++ sections/LearnHero/LearnHero.jsx
@@ -1,19 +1,15 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 import styles from './LearnHero.module.css';
 import Svg from '@/components/Svg';
 import clsx from 'clsx';
 import data from '@/data/learn.yaml';
 
 function LearnHero() {
-  const router = useRouter();
   const titleClasses = clsx('h-500', styles.title);
   const descriptionClasses = clsx('body-long-01', styles.description);
   const dividerClasses = clsx('caption', styles.divider);
   const linkClasses = clsx('btn', styles.link);
   const blockchains = data.resources.map(({ id, name, logo }) => ({ id, name, logo }));
-
-  const scrollIntoView = (id) => router.push(`/learn#${id}`);
 
   return (
     <div className={styles.container}>
@@ -27,13 +23,13 @@ function LearnHero() {
       <div className={dividerClasses}>Select a blockchain to get started</div>
       <div className={styles.blockchainsContainer}>
         {blockchains.map(({ id, name, logo }) => (
-          <div onClick={() => scrollIntoView(id)} className={styles.blockchainCard} key={id}>
+          <a href={`#${id}`} className={styles.blockchainCard} key={id}>
             <Image src={logo} alt={name} width={144} height={144} objectFit="contain" />
-            <a href={`#${id}`} className={linkClasses}>
+            <span className={linkClasses}>
               {name}
               <Svg height="20" width="20" href="/icons/arrow-right-blue.svg" />
-            </a>
-          </div>
+            </span>
+          </a>
         ))}
       </div>
     </div>
