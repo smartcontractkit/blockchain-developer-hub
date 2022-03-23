6aa7308e501236c6c3596d3b390e5c9c5bc3c185
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-23 16:44:42 +0200

    feat: article footer (#113)
    * feat: artickle footer
    
    * fix: ship page

--- layouts/BlogLayout/BlogLayout.jsx
+++ layouts/BlogLayout/BlogLayout.jsx
@@ -3,10 +3,19 @@ import styles from './BlogLayout.module.css';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import clsx from 'clsx';
+import NavLink from '@/components/NavLink';
 
 function BlogLayout({ children, pages }) {
   const router = useRouter();
   const { slug } = router.query;
+
+  const sortedPages = pages?.sort((a, b) => a.data.sidebar_position - b.data.sidebar_position);
+
+  const current_sidebar_position = pages?.find((page) => page.slug === slug).data.sidebar_position;
+
+  const prev_page = sortedPages?.find((page) => page.data.sidebar_position === current_sidebar_position - 1);
+
+  const next_page = sortedPages?.find((page) => page.data.sidebar_position === current_sidebar_position + 1);
 
   return (
     <div className={styles.container}>
@@ -14,7 +23,7 @@ function BlogLayout({ children, pages }) {
         {pages && (
           <>
             <div className={styles.leftSidebar__header}>Getting started</div>
-            {pages.map((page) => (
+            {sortedPages.map((page) => (
               <Link key={page.slug} href={page.slug} passHref>
                 <a className={clsx('btn', styles.leftSidebar__link, slug === page.slug && styles.active)}>
                   {page.data.title}
@@ -26,6 +35,30 @@ function BlogLayout({ children, pages }) {
       </div>
       <div className={styles.content_wrapper}>
         <div className={styles.content}>{children}</div>
+        <div className={styles.footer}>
+          <div>
+            {prev_page && (
+              <NavLink
+                icon="arrow-left.svg"
+                type="outline"
+                iconPosition="left"
+                text={`Previous: ${prev_page.data.title}`}
+                to={prev_page.slug}
+              />
+            )}
+          </div>
+          <div>
+            {next_page && (
+              <NavLink
+                icon="arrow-right.svg"
+                type="outline"
+                iconPosition="right"
+                text={`Next: ${next_page.data.title}`}
+                to={next_page.slug}
+              />
+            )}
+          </div>
+        </div>
       </div>
       <div className={styles.rightSidebar}></div>
     </div>
