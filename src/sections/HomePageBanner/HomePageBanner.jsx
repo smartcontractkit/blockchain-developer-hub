dad3829bc0021d10efb091eeb94b1e6b77870bb4
Mark Shenouda <mark@thisdot.co>
2022-03-24 19:24:18 +0200

    fix: homepage fixes

--- src/sections/HomePageBanner/HomePageBanner.jsx
+++ src/sections/HomePageBanner/HomePageBanner.jsx
@@ -41,7 +41,7 @@ function HomePageBanner() {
               </li>
             </ul>
 
-            <NavLink to={'/learn'} type="primary" text={'Quickstart'} icon="map-pointer.svg" />
+            <NavLink to={'/learn'} type="primary" text="Get Started" icon="map-pointer.svg" />
           </div>
         </div>
         <div className={styles.imageContainer}>
