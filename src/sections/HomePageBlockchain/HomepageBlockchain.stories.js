845b71bb3add5bbc694d818a5b783ff0ca7a7124
Mark Shenouda <mark@thisdot.co>
2022-03-25 13:30:26 +0200

    fix: remove chainlink name

---
+++ src/sections/HomePageBlockchain/HomepageBlockchain.stories.js
@@ -0,0 +1,10 @@
+import HomepageBlockchain from './HomepageBlockchain';
+
+export default {
+  title: 'section/Homepage Blockchain',
+  component: HomepageBlockchain,
+};
+
+const Template = (args) => <HomepageBlockchain {...args} />;
+
+export const Default = Template.bind({});
