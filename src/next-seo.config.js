c418a91ea8e53707b8c1d9a5889649b81d469a41
Mark Shenouda <mark@thisdot.co>
2022-03-28 12:50:55 +0200

    feat: add og image

--- src/next-seo.config.js
+++ src/next-seo.config.js
@@ -6,6 +6,7 @@ export default {
     locale: 'en_US',
     url: 'https://chainlink-developer-hub.vercel.app/',
     site_name: 'Blockchain Devleoper Hub',
+    images: { url: 'https://chainlink-developer-hub.vercel.app/images/og-image.png' },
   },
   twitter: {
     handle: '@handle',
