e13f853f4b0bb367881bc0ef561694b2ba1f3eb9
Mark Shenouda <mark@thisdot.co>
2022-03-28 14:19:11 +0200

    fix: name typo

--- src/next-seo.config.js
+++ src/next-seo.config.js
@@ -1,17 +1,17 @@
 export default {
-  title: 'Blockchain Devleoper Hub',
+  title: 'Blockchain Developer Hub',
   description: 'A short description goes here.',
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: 'https://chainlink-developer-hub.vercel.app/',
-    site_name: 'Blockchain Devleoper Hub',
+    site_name: 'Blockchain Developer Hub',
     images: [
       {
         url: 'https://chainlink-developer-hub.vercel.app/images/og-image.png',
         width: 800,
         height: 600,
-        alt: 'Blockchain Devleoper Hub',
+        alt: 'Blockchain Developer Hub',
       },
     ],
   },
