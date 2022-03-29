6deca3ef71c57faa146218840d12cdfbbb235fda
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-03-29 15:08:16 +0200

    feat: blog layout right sidebar (#166)
    * feat: blog layout right sidebar
    
    * Fix: sticky sidebars, active class
    
    * fix: css
    
    * fix: isElementVisable
    
    Co-authored-by: Jerry Hogan <jerry.hogan@thisdot.co>

---
+++ src/helpers/isElementVisable.js
@@ -0,0 +1,21 @@
+function isElementVisable(el) {
+  let top = el.offsetTop;
+  let left = el.offsetLeft;
+  let width = el.offsetWidth;
+  let height = el.offsetHeight;
+
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    top + height <= window.pageYOffset + window.innerHeight &&
+    left + width <= window.pageXOffset + window.innerWidth
+  );
+}
+
+export default isElementVisable;
