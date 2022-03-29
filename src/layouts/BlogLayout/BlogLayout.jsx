25ab0b8e1b46bbe5a3213a775b951cd6b961b1cc
Mark Shenouda <mark@thisdot.co>
2022-03-29 13:15:35 +0200

    feat: blog layout right sidebar

--- src/layouts/BlogLayout/BlogLayout.jsx
+++ src/layouts/BlogLayout/BlogLayout.jsx
@@ -4,8 +4,32 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import clsx from 'clsx';
 import NavLink from '@/components/NavLink';
+import { useEffect, useState } from 'react';
+
+function elementInViewport(el) {
+  let top = el.offsetTop;
+  let left = el.offsetLeft;
+  let width = el.offsetWidth;
+  let height = el.offsetHeight;
+
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    top + height <= window.pageYOffset + window.innerHeight &&
+    left + width <= window.pageXOffset + window.innerWidth
+  );
+}
 
 function BlogLayout({ children, pages }) {
+  const [headings, setHeadings] = useState([]);
+  const [activeHeading, setActiveHeading] = useState(null);
+
   const router = useRouter();
   const { slug } = router.query;
 
@@ -17,12 +41,33 @@ function BlogLayout({ children, pages }) {
 
   const next_page = sortedPages?.find((page) => page.data.sidebar_position === current_sidebar_position + 1);
 
+  useEffect(() => {
+    const headingsElements = document.querySelectorAll('h2');
+    setHeadings(Array.from(headingsElements));
+
+    document.addEventListener('scroll', () => {
+      const activeHeadingElement = headings.find((heading) => elementInViewport(heading));
+      setActiveHeading(activeHeadingElement);
+    });
+
+    return () => {
+      document.removeEventListener('scroll', () => {
+        const activeHeadingElement = headings.find((heading) => elementInViewport(heading));
+        setActiveHeading(activeHeadingElement);
+      });
+    };
+  }, [children]);
+
+  useEffect(() => {}, [headings]);
+
+  useEffect(() => console.log(activeHeading), [activeHeading]);
+
   return (
     <div className={styles.container}>
       <div className={styles.leftSidebar}>
         {pages && (
           <>
-            <div className={styles.leftSidebar__header}>Getting started</div>
+            <div className={styles.sidebar__header}>Getting started</div>
             {sortedPages.map((page) => (
               <Link key={page.slug} href={page.slug} passHref>
                 <a className={clsx('btn', styles.leftSidebar__link, slug === page.slug && styles.active)}>
@@ -60,7 +105,14 @@ function BlogLayout({ children, pages }) {
           </div>
         </div>
       </div>
-      <div className={styles.rightSidebar}></div>
+      <div className={styles.rightSidebar}>
+        <div className={styles.sidebar__header}>On this page</div>
+        {headings.map((heading) => (
+          <Link key={heading.id} href={`#${heading.id}`} passHref>
+            <a className={clsx('caption', styles.rightSidebar__link, styles.active)}>{heading.innerHTML}</a>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
