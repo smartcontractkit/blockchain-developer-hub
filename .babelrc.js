587a54633c1cc6e2bf142747fa5df3ab7a637c7b
Mark Shenouda <mark@thisdot.co>
2022-03-30 12:14:56 +0200

    feat: install jest and add markdown tests

---
+++ .babelrc.js
@@ -0,0 +1,5 @@
+const isTest = String(process.env.NODE_ENV) === 'test';
+
+module.exports = {
+  presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }], '@babel/preset-react'],
+};
---
+++ data/pages/__tests__/MDX.test.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+describe('MDX content', () => {
+  const blockchain101files = fs.readdirSync(path.join(`data/pages/blockchain101`));
+  const files = ['ship.md', ...blockchain101files.map((filename) => `blockchain101/${filename}`)];
+
+  test.each(files)(`%s has a heading`, (file) => {
+    const filePath = path.join(`data/pages/${file}`);
+    const content = fs.readFileSync(filePath, 'utf8');
+
+    const h1Regex = /<h1>.*<\/h1>/g;
+    const markdownH1Regex = /^#[^#].*/gm;
+
+    expect(content).not.toMatch(h1Regex);
+    expect(content).not.toMatch(markdownH1Regex);
+  });
+});
