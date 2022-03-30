2e0a9f7dc066a19a267651c2af2c68b208ac185d
Mark Shenouda <mark@thisdot.co>
2022-03-30 12:16:50 +0200

    fix: remove babel

--- .babelrc.js
+++
@@ -1,5 +0,0 @@
-const isTest = String(process.env.NODE_ENV) === 'test';
-
-module.exports = {
-  presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }], '@babel/preset-react'],
-};
