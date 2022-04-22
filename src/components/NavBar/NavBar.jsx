e7ace8a526d8609c80e81f791c6150d298bd7f35
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-22 14:19:33 +0100

    Feat: Mobile menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import NavLink from '@/components/NavLink';
 import styles from './NavBar.module.css';
+import clsx from 'clsx';
+import { useState } from 'react';
 
 const links = [
   {
@@ -27,6 +29,7 @@ const links = [
 ];
 
 export default function NavBar() {
+  const [showNavLinks, setShowNavLinks] = useState(false);
   const { pathname } = useRouter();
   const isActive = (path, to) => {
     if (path.split('/')[1] === to.split('/')[1]) {
@@ -34,17 +37,25 @@ export default function NavBar() {
     }
     return false;
   };
+  const toggleMenuLink = (value) => {
+    document.body.style.overflow = value ? 'hidden' : 'auto';
+    setShowNavLinks(value);
+  };
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
+        <div className={clsx({ [styles.mobile_menu_overlay]: showNavLinks })} onClick={() => toggleMenuLink(false)} />
         <div className={styles.nav_contents}>
+          <button className={styles.menu_btn} onClick={() => toggleMenuLink(!showNavLinks)}>
+            <img src={`/icons/${showNavLinks ? 'close.png' : 'menu.png'}`} atl="menu icon" />
+          </button>
           <Link href="/">
             <a className={styles.logo}>Blockchain Developer Hub</a>
           </Link>
 
-          <ul className={styles.nav_links}>
+          <ul className={clsx(styles.nav_links, { [styles.visible]: showNavLinks })}>
             {links.map((res, index) => (
-              <li key={index}>
+              <li key={index} onClick={() => toggleMenuLink(false)}>
                 <NavLink text={res.text} to={res.to} type="link" active={isActive(pathname, res.to)} />
               </li>
             ))}
