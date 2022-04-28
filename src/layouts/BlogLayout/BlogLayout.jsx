44dbad2b04171d42a90b235bdf3df502342c5fe6
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-28 09:57:20 +0100

    Still need clarification on a design

--- src/layouts/BlogLayout/BlogLayout.jsx
+++ src/layouts/BlogLayout/BlogLayout.jsx
@@ -24,6 +24,10 @@ function BlogLayout({ children, pages }) {
 
   const next_page = sortedPages?.find((page) => page.data.sidebar_position === current_sidebar_position + 1);
 
+  const toggleArticleMenu = (value) => {
+    setArticleOverview(value);
+  };
+
   useEffect(() => {
     const headingsElements = Array.from(document.querySelectorAll('h2'));
     setHeadings(headingsElements);
@@ -43,84 +47,89 @@ function BlogLayout({ children, pages }) {
   }, [children]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.leftSidebar}>
-        {pages && (
-          <>
-            <div className={styles.sidebar__header}>Getting started</div>
-            {sortedPages.map((page) => (
-              <Link key={page.slug} href={page.slug} passHref>
-                <a className={clsx('btn', styles.leftSidebar__link, slug === page.slug && styles.active)}>
-                  {page.data.title}
-                </a>
-              </Link>
-            ))}
-          </>
-        )}
-      </div>
-      <div className={styles.content_wrapper}>
-        <div className={styles.content} id="blog-content">
-          {children}
-        </div>
-        <div className={styles.footer}>
-          <div>
-            {prev_page && (
-              <NavLink
-                icon="arrow-left.svg"
-                type="outline"
-                iconPosition="left"
-                text={`Previous: ${prev_page.data.title}`}
-                to={prev_page.slug}
-              />
-            )}
-          </div>
-          <div>
-            {next_page && (
-              <NavLink
-                icon="arrow-right.svg"
-                type="outline"
-                iconPosition="right"
-                text={`Next: ${next_page.data.title}`}
-                to={next_page.slug}
-              />
-            )}
-          </div>
-
-          {!next_page && (
-            <div>
-              <NavLink
-                icon="arrow-right.svg"
-                type="outline"
-                iconPosition="right"
-                text={`Next: Go to Learn`}
-                to={'/learn'}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className={styles.rightSidebar}>
-        <div className={styles.sidebar__header}>On this page</div>
-        {headings.map((heading) => (
-          <Link key={heading.id} href={`#${heading.id}`} passHref>
-            <a
-              className={clsx('caption', styles.rightSidebar__link, {
-                [styles.active]: activeHeading && heading.id === activeHeading.id,
-              })}
-            >
-              {heading.innerHTML}
-            </a>
-          </Link>
-        ))}
-      </div>
-      <Overlay showOverlay={articleOverview} toggleMenu={() => {}} />
+    <>
+      <Overlay showOverlay={articleOverview} isOverview={true} toggleMenu={toggleArticleMenu} />
       {pages && (
         <button className={clsx(styles.overview_mobile_btn, 'btn')} onClick={() => setArticleOverview(true)}>
           <span>Articles overview</span>
           <img src="/icons/dropdown-blue.svg" alt="dropdown icon" />
         </button>
       )}
-    </div>
+      <div className={styles.container}>
+        <div className={clsx(styles.leftSidebar, { [styles.mobile]: articleOverview })}>
+          {pages && (
+            <>
+              <div className={styles.sidebar__header}>Getting started</div>
+              {sortedPages.map((page) => (
+                <Link key={page.slug} href={page.slug} passHref>
+                  <a
+                    onClick={() => toggleArticleMenu(false)}
+                    className={clsx('btn', styles.leftSidebar__link, slug === page.slug && styles.active)}
+                  >
+                    {page.data.title}
+                  </a>
+                </Link>
+              ))}
+            </>
+          )}
+        </div>
+        <div className={styles.content_wrapper}>
+          <div className={styles.content} id="blog-content">
+            {children}
+          </div>
+          <div className={styles.footer}>
+            <div>
+              {prev_page && (
+                <NavLink
+                  icon="arrow-left.svg"
+                  type="outline"
+                  iconPosition="left"
+                  text={`Previous: ${prev_page.data.title}`}
+                  to={prev_page.slug}
+                />
+              )}
+            </div>
+            <div>
+              {next_page && (
+                <NavLink
+                  icon="arrow-right.svg"
+                  type="outline"
+                  iconPosition="right"
+                  text={`Next: ${next_page.data.title}`}
+                  to={next_page.slug}
+                />
+              )}
+            </div>
+
+            {!next_page && (
+              <div>
+                <NavLink
+                  icon="arrow-right.svg"
+                  type="outline"
+                  iconPosition="right"
+                  text={`Next: Go to Learn`}
+                  to={'/learn'}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className={styles.rightSidebar}>
+          <div className={styles.sidebar__header}>On this page</div>
+          {headings.map((heading) => (
+            <Link key={heading.id} href={`#${heading.id}`} passHref>
+              <a
+                className={clsx('caption', styles.rightSidebar__link, {
+                  [styles.active]: activeHeading && heading.id === activeHeading.id,
+                })}
+              >
+                {heading.innerHTML}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
