3438e70db5b923732ee3f9ec5f54b96fbf4fc2e3
Jerry Hogan <jerry.hogan@thisdot.co>
2022-05-05 09:51:20 +0100

    Feat: storybook reflect style, styles for quote and code added

--- src/stories/MarkdownStyle/MarkdownStyles.stories.js
+++ src/stories/MarkdownStyle/MarkdownStyles.stories.js
@@ -2,7 +2,7 @@ import styles from '@/styles/MDX.module.css';
 import MdxContent from './MarkdownContent.md';
 
 export default {
-  title: 'Mdx/ without mdx file style',
+  title: 'Markdown/Default',
 };
 
 const Template = () => (
