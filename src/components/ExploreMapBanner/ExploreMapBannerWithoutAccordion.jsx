6655f1405e1906f8924222d8a31c5b4aa887953c
Jerry Hogan <jerry.hogan@thisdot.co>
2022-05-17 11:37:34 +0100

    Reverted ecosystem to previous design (#217)
    * Reverted ecosystem to previous design
    
    * Reverted ecosystem to previous design
    
    * explore map url changed

---
+++ src/components/ExploreMapBanner/ExploreMapBannerWithoutAccordion.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import NavLink from '@/components/NavLink';
+
+import styles from './ExploreMapBanner.module.css';
+
+export default function ExploreMapBanner({ isHomePage, title, description }) {
+  const titleClasses = clsx('h-500', styles.title);
+  const descriptionClasses = clsx('body-long-01', styles.description);
+
+  return (
+    <div className={styles.container}>
+      {isHomePage ? <h2 className={titleClasses}>{title}</h2> : <h1 className={titleClasses}>{title}</h1>}
+      <p className={descriptionClasses}>{description}</p>
+      <div className={styles.btns}>
+        <NavLink
+          to={
+            'https://www.figma.com/proto/U6iAHSsDlqoJfOaNwRigeO/Ecosystem-Tools-Map?page-id=0%3A1&node-id=2%3A1241&viewport=264%2C48%2C0.68&scaling=scale-down'
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+          type="primary"
+          text={'Explore Map'}
+          iconPosition="right"
+          icon="map-pointer.svg"
+        />
+        {!isHomePage && (
+          <NavLink
+            to={'https://developer-roadmap-1.vercel.app/blockchain'}
+            target="_blank"
+            rel="noopener noreferrer"
+            type="outline"
+            text={'Roadmap'}
+            iconPosition="right"
+            icon="map.svg"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+ExploreMapBanner.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isHomePage: PropTypes.bool,
+};
+
+ExploreMapBanner.defaultProps = {
+  isHomePage: true,
+};
--- src/components/ExploreMapBanner/index.js
+++ src/components/ExploreMapBanner/index.js
@@ -1,1 +1,1 @@
-export { default as default } from './ExploreMapBanner';
+export { default as default } from './ExploreMapBannerWithoutAccordion';
