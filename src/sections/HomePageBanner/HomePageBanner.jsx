a9963c0e14b17d5f71259243185db7bb74cd936f
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-22 14:27:36 +0100

    in progress

--- src/sections/HomePageBanner/HomePageBanner.jsx
+++ src/sections/HomePageBanner/HomePageBanner.jsx
@@ -1,56 +1,18 @@
-import NavLink from '@/components/NavLink';
-import Image from 'next/image';
+// import NavLink from '@/components/NavLink';
+// import Image from 'next/image';
 import styles from './HomePageBanner.module.css';
-import clsx from 'clsx';
-import Svg from '@/components/Svg';
+// import clsx from 'clsx';
+// import Svg from '@/components/Svg';
 
 function HomePageBanner() {
-  const titleClasses = clsx('h-500', styles.title);
+  // const titleClasses = clsx('h-500', styles.title);
 
   return (
     <section className={styles.container}>
-      <div className={styles.banner}>
-        <div className={styles.banner_left}>
-          <div>
-            <h1 className={titleClasses}>
-              <p className={styles.title_a}>
-                <span className={styles.arrow}>{'-->'}</span>
-                <span>{'Dive into'}</span>
-              </p>
-              <p>{'smart contract development'}</p>
-            </h1>
-
-            <ul className={styles.list}>
-              <li className={clsx('body-short-01', styles.list_item)}>
-                <Svg className={styles.list_icon} height="24" width="24" href="/icons/school.svg" title="school icon" />
-                <small>
-                  <strong>Learn the essentials</strong> of blockchain and smart contract development.
-                </small>
-              </li>
-              <li className={clsx('body-short-01', styles.list_item)}>
-                <Svg className={styles.list_icon} height="24" width="24" href="/icons/memory.svg" title="memory icon" />
-                <small>
-                  <strong>Get familiar</strong> with the tools and resources you need to start building your own
-                  project.
-                </small>
-              </li>
-              <li className={clsx('body-short-01', styles.list_item)}>
-                <Svg className={styles.list_icon} height="24" width="24" href="/icons/map.svg" title="map icon" />
-                <small>
-                  <strong>Lay the foundations</strong> for your journey to becoming a blockchain developer.
-                </small>
-              </li>
-            </ul>
-
-            <NavLink to={'/blockchain101/blockchain'} type="primary" text="Get Started" />
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <div className={styles.image}>
-            <Image src={'/demo/banner-right.png'} alt={'Banner image'} layout="fill" objectFit="contain" />
-          </div>
-        </div>
-      </div>
+      <h1 className={styles.heading}>
+        Jump into <span className={styles.smart_text}>smart</span> <br />{' '}
+        <span className={styles.contracts_text}>contracts</span> development
+      </h1>
     </section>
   );
 }
