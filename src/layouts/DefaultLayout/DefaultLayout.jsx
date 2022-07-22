f2a22a2c07c0fb38bd23406b151e5367376b64be
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-22 09:07:11 +0100

    Update favicon (#281)
    * chore: faviocn changed, icons generated also
    
    * put icons in images folder
    
    * added the icon to the head
    
    * removed safari pinned

--- src/layouts/DefaultLayout/DefaultLayout.jsx
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,9 +1,17 @@
 import NavBar from '@/components/NavBar';
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 
 export default function DefaultLayout({ children }) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/images/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/images/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+      </Head>
       <NavBar />
       <main>{children}</main>
     </>
