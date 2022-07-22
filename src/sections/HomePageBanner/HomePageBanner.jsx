57510054a6449f40a57ad0f3d1290981fbbb4c08
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-22 16:44:17 +0100

    chore: hero banner completed, color text still not as expected

--- src/sections/HomePageBanner/HomePageBanner.jsx
+++ src/sections/HomePageBanner/HomePageBanner.jsx
@@ -1,18 +1,33 @@
-// import NavLink from '@/components/NavLink';
+import NavLink from '@/components/NavLink';
 // import Image from 'next/image';
 import styles from './HomePageBanner.module.css';
-// import clsx from 'clsx';
+import clsx from 'clsx';
 // import Svg from '@/components/Svg';
 
 function HomePageBanner() {
-  // const titleClasses = clsx('h-500', styles.title);
+  const headingClasses = clsx('h-600', styles.heading);
+  const descriptionClasses = clsx('text-lg--long', styles.description);
 
   return (
     <section className={styles.container}>
-      <h1 className={styles.heading}>
-        Jump into <span className={styles.smart_text}>smart</span> <br />{' '}
-        <span className={styles.contracts_text}>contracts</span> development
-      </h1>
+      <div className={styles.contents}>
+        <h1 className={headingClasses}>
+          Jump into <span className={styles.smart_text}>smart</span>{' '}
+          <span className={styles.contracts_text}>contracts</span> development
+        </h1>
+        <div className={descriptionClasses}>
+          Learn the essentials, become familiar with popular dev stacks, and define your journey to becoming a
+          blockchain developer.
+        </div>
+        <NavLink
+          to={'/blockchain101/blockchain'}
+          type="link"
+          text="Start here"
+          iconPosition="right"
+          icon="arrow-right-rounded-green.svg"
+          className="btn--extra-bold"
+        />
+      </div>
     </section>
   );
 }
