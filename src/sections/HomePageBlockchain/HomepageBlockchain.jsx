39f9ac629e7e699adb2f80916b29a10dccc1e0c7
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-25 10:30:20 +0100

    in progress

--- src/sections/HomePageBlockchain/HomepageBlockchain.jsx
+++ src/sections/HomePageBlockchain/HomepageBlockchain.jsx
@@ -1,27 +1,25 @@
-import clsx from 'clsx';
-import QuoteCard from '@/components/QuoteCard';
+// import QuoteCard from '@/components/QuoteCard';
 import styles from './HomepageBlockchain.module.css';
-import Svg from '@/components/Svg';
 import data from '@/data/home.yaml';
+import CardWithArrow from '@/components/CardWithArrow';
+import padNumber from '@/helpers/padNumber';
 
 function HomepageBlockchain() {
-  const quoteClasses = clsx('body-short-01--bold', styles.quote);
-  const labelClasses = clsx('body-long-01', styles.label);
-
   const cardItems = data.hero.featured_articles;
 
   return (
     <div className={styles.container}>
-      <QuoteCard cards={cardItems} textDirection="right">
-        <div className={styles.quote_wrapper}>
-          <Svg height="30" width="20" href="/icons/quote.svg" title="quote icon" />
-          <p className={quoteClasses}>
-            Blockchain development skills are rated as the fastest-growing in-demand skills of 2020, with an increase of
-            6000%
-          </p>
-          <small className={labelClasses}>blockchainjobs.com</small>
-        </div>
-      </QuoteCard>
+      <div className={styles.cards}>
+        {cardItems.map((card, index) => (
+          <CardWithArrow
+            href={card.href}
+            title={card.title}
+            description={card.description}
+            subtitle={padNumber(index, 9)}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
