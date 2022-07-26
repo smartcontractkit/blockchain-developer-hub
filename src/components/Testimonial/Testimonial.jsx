a1e271f56b96a9ae8df0df8cad3c126910b56004
Ian Mungai <iansam74@gmail.com>
2022-07-26 16:38:43 +0300

    chore: testimonial design

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 import Svg from '@/components/Svg';
+import styles from './Testimonial.module.css';
 
 function Testimonial({ title, content, source, icon }) {
   return (
-    <>
-      <div>{title}</div>
-      <div>{content}</div>
-      <div>
-        source: <span>{source}</span>
+    <div className={styles.card}>
+      <div className={styles.card__inner}>
+        <div className={styles.card__info}>
+          <span className={styles.title}>{title}</span>
+          <span className={styles.content}>{content}</span>
+          <span className={styles.source}>
+            source: <span>{source}</span>
+          </span>
+        </div>
+        <div className={styles.icon__container}>
+          <Svg height="64" width="64" href={icon} title="testimonial icon" />
+        </div>
       </div>
-      <div>
-        <Svg height="18" width="18" href={icon} title="testimonial icon" />
-      </div>
-    </>
+    </div>
   );
 }
 
