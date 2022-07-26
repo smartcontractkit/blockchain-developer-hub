226e7bf8654c452458f5ec071e4e0adc0a021af7
Ian Mungai <iansam74@gmail.com>
2022-07-26 17:07:18 +0300

    fix: content style

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
 import Svg from '@/components/Svg';
+import clsx from 'clsx';
 import styles from './Testimonial.module.css';
 
-function Testimonial({ title, content, source, icon }) {
+function Testimonial({ title, source, icon }) {
   return (
     <div className={styles.card}>
       <div className={styles.card__inner}>
         <div className={styles.card__info}>
           <span className={styles.title}>{title}</span>
-          <span className={styles.content}>{content}</span>
+          <span className={clsx('subtitle-01', styles.content)}>
+            <span>Blockchain development skills are rated as the fastest-growing in-demand skills, with an</span> increase of 60x <span>in 2020.</span>
+          </span>
           <span className={styles.source}>
             source: <span>{source}</span>
           </span>
@@ -23,7 +26,7 @@ function Testimonial({ title, content, source, icon }) {
 
 Testimonial.propTypes = {
   title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
+  content: PropTypes.string,
   source: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
 };
