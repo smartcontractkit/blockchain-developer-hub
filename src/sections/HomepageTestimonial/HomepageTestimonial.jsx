3c861415e8e66e0a4b2bb92e67d374709fb65261
Ian Mungai <iansam74@gmail.com>
2022-07-26 15:10:37 +0300

    chore: setup data source testimonial

--- src/sections/HomepageTestimonial/HomepageTestimonial.jsx
+++ src/sections/HomepageTestimonial/HomepageTestimonial.jsx
@@ -1,14 +1,19 @@
 import Testimonial from '@/components/Testimonial';
 import styles from './HomepageTestimonial.module.css';
+import data from '@/data/home.yaml';
 
 function HomepageTestimonial() {
+  const testimonial = data.testimonial;
+
+  console.log('testimonial', testimonial);
+
   return (
     <div className={styles.container}>
       <Testimonial
-        title="Did you know that..."
-        content="Blockchain development skills are rated as the fastest-growing in-demand skills, with an increase of 60x in 2020."
-        source="source: blockchainjobs.com"
-        icon="chart.svg"
+        title={testimonial.title}
+        content={testimonial.content}
+        source={testimonial.source}
+        icon={testimonial.icon}
       />
     </div>
   );
