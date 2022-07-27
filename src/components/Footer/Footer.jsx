129b5d2f5fd1c358e827e903cb56ccbbe2782aa3
Ian Sam Mungai <iansam74@gmail.com>
2022-07-27 13:16:20 +0300

    update typography

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={styles.container}>
       <img src="/images/logo-footer.png" alt="blockchain logo" />
-      <p className={clsx('.text-md--short', styles.copyright)}>{new Date().getFullYear()} @Blockchain Education</p>
+      <p className={clsx('text-md--short', styles.copyright)}>{new Date().getFullYear()} @Blockchain Education</p>
     </footer>
   );
 }
