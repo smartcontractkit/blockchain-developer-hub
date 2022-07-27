96d1a9edadddd68fc5b42b3e38c401fdfa5d5176
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-27 12:13:46 +0100

    clean ups next

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import HomeExplore from '@/sections/HomeExplore';
 import HomePageBanner from '@/sections/HomePageBanner';
 import HomepageBlockchain from '@/sections/HomePageBlockchain';
 import HomePageImproveSkillsStories from '@/sections/HomePageImproveSkills';
-import HomePageLifecycleBanner from '@/sections/HomePageLifecycleBanner';
 
 export default function Home() {
   return (
@@ -11,12 +10,6 @@ export default function Home() {
       <HomePageBanner />
       <HomepageBlockchain />
       <HomeExplore />
-      <HomePageLifecycleBanner
-        title="Get to know the development cycle"
-        overview="The ecosystem tools map outlines the entire smart contract developer ecosystem, and all of the tools and products smart contract developers may need to rely on."
-        linkText="Explore Map"
-        link="https://smartcontractecosystem.com/"
-      />
       <CaseStudyBanner />
       <HomePageImproveSkillsStories />
     </div>
--- src/sections/CaseStudyBanner/CaseStudyBanner.jsx
+++ src/sections/CaseStudyBanner/CaseStudyBanner.jsx
@@ -6,21 +6,19 @@ export default function CaseStudyBanner() {
   return (
     <div className={styles.container}>
       <div className={styles.content_wrapper}>
-        <img
-          src="https://cdn.consensys.net/uploads/2021/09/16181652/damien-1.7923f061-958x460.png"
-          alt="Case study Banner image"
-          className={styles.image}
-        />
+        <div className={styles.image}>
+          <img src="/images/ecosystem-map.png" alt="Case study Banner image" />
+        </div>
 
         <div className={styles.info}>
           <span className={clsx('overline--extra-bold', styles.subtitle)}>smart contracts ecosystem</span>
           <h3 className={clsx('h-200', styles.title)}>A growing landscape of tools and technologies</h3>
-          <p className={clsx('body-long-01', styles.overview)}>
+          <p className={clsx('text-lg--long', styles.overview)}>
             All of the tools and products smart contract developers may need to rely on gathered in one place.
           </p>
           <div className={styles.actions}>
             <NavLink
-              to="https://consensys.net/blog/nft/over-32000-users-from-130-countries-applied-to-purchase-damien-hirsts-nfts-on-the-palm-network/"
+              to="https://www.figma.com/proto/U6iAHSsDlqoJfOaNwRigeO/Ecosystem-Tools-Map?page-id=0%3A1&node-id=2%3A1241&viewport=264%2C48%2C0.68&scaling=scale-down"
               target="_blank"
               rel="noopener noreferrer"
               type="link"
