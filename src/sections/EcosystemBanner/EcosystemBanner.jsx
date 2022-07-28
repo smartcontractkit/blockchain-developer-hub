9d830234717d0a3a4d5809b35d333bd12b988502
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-28 10:14:35 +0100

    chore: whats left is ensuring the bg image seats properly

--- src/sections/EcosystemBanner/EcosystemBanner.jsx
+++ src/sections/EcosystemBanner/EcosystemBanner.jsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
-import NavLink from '@/components/NavLink';
 import styles from './EcosystemBanner.module.css';
+import ExploreMapBanner from '@/components/ExploreMapBanner';
 
 export default function CaseStudyBanner() {
   return (
@@ -12,21 +12,12 @@ export default function CaseStudyBanner() {
 
         <div className={styles.info}>
           <span className={clsx('overline--extra-bold', styles.subtitle)}>smart contracts ecosystem</span>
-          <h3 className={clsx('h-200', styles.title)}>A growing landscape of tools and technologies</h3>
-          <p className={clsx('text-lg--long', styles.overview)}>
-            All of the tools and products smart contract developers may need to rely on gathered in one place.
-          </p>
-          <div className={styles.actions}>
-            <NavLink
-              to="https://www.figma.com/proto/U6iAHSsDlqoJfOaNwRigeO/Ecosystem-Tools-Map?page-id=0%3A1&node-id=2%3A1241&viewport=264%2C48%2C0.68&scaling=scale-down"
-              target="_blank"
-              rel="noopener noreferrer"
-              type="link"
-              icon="grid.svg"
-              iconPosition="left"
-              text={'Explore Ecosystem'}
-            />
-          </div>
+          <ExploreMapBanner
+            description="All of the tools and products smart contract developers may need to rely on gathered in one place."
+            isHomePage={true}
+          >
+            <h3 className={clsx('h-200', styles.title)}>A growing landscape of tools and technologies</h3>
+          </ExploreMapBanner>
         </div>
       </div>
     </div>
