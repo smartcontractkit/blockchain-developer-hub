c6a92b1a6b81b7678320a7e7ca6301bec9dd6b71
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-11 18:22:00 +0100

    chore: navbar updated (#340)
    * chore: navbar updated
    
    * updated github logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,36 +6,19 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { useState, useRef, useEffect } from 'react';
 import Overlay from '@/components/Overlay';
+import navbarLinks from '@/data/navbarLinks.yaml';
 
-const links = [
-  {
-    to: '/blockchain101/blockchain',
-    text: 'Blockchain 101',
-  },
-  {
-    to: '/learn',
-    text: 'Learn',
-  },
-  {
-    to: '/ecosystem-map',
-    text: 'Ecosystem',
-  },
-  {
-    to: '/build',
-    text: 'Build',
-  },
-  {
-    to: '/ship',
-    text: 'Ship',
-  },
-];
+import GithubLogo from 'public/icons/github.svg';
+
+const links = navbarLinks.items;
 
 export default function NavBar({ isSticky }) {
   const bodyRef = useRef();
   const [showNavLinks, setShowNavLinks] = useState(false);
+  const [isBlogPage, setIsBlogPage] = useState(false);
   const { pathname } = useRouter();
-  const isActive = (path, to) => {
-    if (path.split('/')[1] === to.split('/')[1]) {
+  const isActive = (path, href) => {
+    if (path.split('/')[1] === href.split('/')[1]) {
       return true;
     }
     return false;
@@ -46,11 +29,17 @@ export default function NavBar({ isSticky }) {
   };
 
   useEffect(() => {
+    const blogLinks = ['/blockchain101', '/ship'];
+    const result = blogLinks.find((res) => pathname.match(new RegExp(res, 'gi')));
+    setIsBlogPage(result ? true : false);
+  }, [pathname]);
+
+  useEffect(() => {
     bodyRef.current = document.body;
   }, []);
   return (
     <nav className={clsx(styles.nav, { [styles.skicky]: isSticky })}>
-      <div className={styles.container}>
+      <div className={clsx(styles.container, { [styles.blog]: isBlogPage })}>
         <Overlay showOverlay={showNavLinks} toggleMenu={toggleMenuLink} />
         <div className={styles.nav_contents}>
           <button className={styles.menu_btn} onClick={() => toggleMenuLink(!showNavLinks)}>
@@ -58,19 +47,14 @@ export default function NavBar({ isSticky }) {
           </button>
           <Link href="/">
             <a className={styles.logo} onClick={() => toggleMenuLink(false)}>
-              <img src="/images/favicon-32x32.png" alt="blockchain logo" />
-              <span className="text-lg--short-semi">
-                blockchain
-                <br />
-                education
-              </span>
+              <img src="/logos/logo.png" alt="blockchain logo" />
             </a>
           </Link>
 
           <ul className={clsx(styles.nav_links, { [styles.mobile]: showNavLinks })}>
             {links.map((res, index) => (
               <li key={index} onClick={() => toggleMenuLink(false)}>
-                <NavLink text={res.text} to={res.to} type="link" active={isActive(pathname, res.to)} />
+                <NavLink text={res.name} to={res.href} type="link" active={isActive(pathname, res.href)} />
               </li>
             ))}
           </ul>
@@ -80,8 +64,8 @@ export default function NavBar({ isSticky }) {
             rel="noopener noreferrer"
             className={styles.githublogo}
           >
-            <span className="btn--extra-bold">Contribute</span>
-            <img src="/icons/github.svg" width="24" height="24" alt="github logo" />
+            <span className="btn-sm--extra-bold">Contribute</span>
+            <GithubLogo />
           </a>
         </div>
       </div>
