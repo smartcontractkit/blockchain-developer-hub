7484ceec313acfa07ff3b38fd3fccc128ca30fb1
Jerry Hogan <jerry.hogan@thisdot.co>
2022-09-15 17:52:32 +0100

    chore: moved case studies in build to its own page, created yaml file for housing case studies

--- src/pages/case-studies/index.js
+++ src/pages/case-studies/index.js
@@ -1,18 +1,19 @@
 import PagePagination from '@/components/PagePagination';
-import data from '@/data/build.yaml';
+import data from '@/data/case-studies.yaml';
 import removePastEvent from '@/helpers/removePastEvent';
 import BuildPageSection from '@/sections/BuildPageSection';
 import styles from '@/styles/pages/BuildPage.module.css';
-function Build() {
+
+function CaseStudies() {
   const { categories } = data;
   return (
     <div className={styles.container}>
       {categories.map(({ name, overview, items, href }, index) => (
         <BuildPageSection key={index} name={name} overview={overview} href={href} items={removePastEvent(items)} />
       ))}
-      <PagePagination hasPadding={false} text="Case Studies" link="/case-studies" />
+      <PagePagination hasPadding={false} text="Ship" link="/ship" />
     </div>
   );
 }
 
-export default Build;
+export default CaseStudies;
