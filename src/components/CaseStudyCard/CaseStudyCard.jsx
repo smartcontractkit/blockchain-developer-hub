14c824a334e49142d61ef8a482f4f9dbd274e7e5
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-07 11:22:53 +0100

    chore:  responsiveness completed

--- src/components/CaseStudyCard/CaseStudyCard.jsx
+++ src/components/CaseStudyCard/CaseStudyCard.jsx
@@ -2,26 +2,34 @@ import React from 'react';
 import styles from './CaseStudyCard.module.css';
 import PropTypes from 'prop-types';
 import getTags from '@/helpers/getTags';
+import clsx from 'clsx';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function CaseStudyCard({ tags, image, title }) {
+export default function CaseStudyCard({ tags, image, title, href }) {
   const tagLists = getTags(tags);
+  const tagClasses = clsx('body-short-03', styles.tag);
+  const titleClasses = clsx('text-lg--short-semi');
+  const itsExternalLink = href.startsWith('https://') || href.startsWith('http://') ? true : false;
 
   return (
-    <div className={styles.card}>
-      <div className={styles.card_heading}>
-        <div className={styles.tags}>
-          {tagLists.map((tag, index) => (
-            <span className={styles.tag} key={index}>
-              {tag}
-            </span>
-          ))}
+    <Link href={href} passHref>
+      <a className={styles.card} target={itsExternalLink ? '_blank' : null} rel="noopener noreferrer">
+        <div className={styles.card_heading}>
+          <div className={styles.tags}>
+            {tagLists.map((tag, index) => (
+              <span className={tagClasses} key={index}>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <div className={titleClasses}>{title}</div>
         </div>
-        <div>{title}</div>
-      </div>
-      <div className={styles.card_image}>
-        <img src={image} alt="case image" />
-      </div>
-    </div>
+        <div className={styles.card_image}>
+          <Image src={image} alt="case image" layout="fill" />
+        </div>
+      </a>
+    </Link>
   );
 }
 
