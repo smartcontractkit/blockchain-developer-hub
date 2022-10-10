8341d613c9b146c47bc65b2360ca1ae18b22ea77
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-10 16:09:52 +0100

    chore: test subscribe api, added url to env, restructure code

--- src/pages/api/newsletter/subscribe.js
+++ src/pages/api/newsletter/subscribe.js
@@ -1,67 +1,62 @@
 /* eslint-disable no-useless-escape */
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    // Validate request body
-    if (!req.body.email) {
-      res.status(400).send('Missing email');
-      return;
-    }
+  const { NEXT_HUBSPOT_API_KEY, NEXT_HUBSPOT_API_URL } = process.env;
 
-    // Validate email
-    const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!emailRegex.test(req.body.email)) {
-      res.status(400).send('Invalid email');
-      return;
-    }
+  if (NEXT_HUBSPOT_API_KEY && NEXT_HUBSPOT_API_URL) {
+    if (req.method === 'POST') {
+      // Validate request body
+      if (!req.body.email) {
+        res.status(400).send('Missing email');
+        return;
+      }
 
-    // Subscribe user
-    await fetch(`https://api.hubapi.com/contacts/v1/contact/?hapikey=${process.env.HUBSPOT_API_KEY}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify({
-        properties: [{ property: 'email', value: req.body.email }],
-      }),
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          /*
-           * The code below has been generated automatically by Github Copilot.
-           * To be honest I wanna keep it because it looks useful and will save hours of searching in Hubspot docs
-           * when we need to add this feature.
-           */
+      // Validate email
+      const emailRegex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!emailRegex.test(req.body.email)) {
+        res.status(400).send('Invalid email');
+        return;
+      }
 
-          // Send confirmation email
-          // fetch(`https://api.hubapi.com/email/public/v1/singleEmail/send?hapikey=${process.env.HUBSPOT_API_KEY}`, {
-          //   method: 'POST',
-          //   headers: {
-          //     'Content-Type': 'application/json',
-          //   },
-          //   redirect: 'follow',
-          //   referrerPolicy: 'no-referrer',
-          //   body: JSON.stringify({
-          //     emailId: process.env.HUBSPOT_EMAIL_ID,
-          //     to: req.body.email,
-          //     mergeVars: {
-          //       EMAIL: req.body.email,
-          //     },
-          //   }),
-          // });
-
-          res.status(200).send('Subscribed');
-        } else {
-          res.status(response.status).send('Something went wrong');
-        }
+      // Subscribe user
+      await fetch(NEXT_HUBSPOT_API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${NEXT_HUBSPOT_API_KEY}`,
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({
+          properties: [{ property: 'email', value: req.body.email }],
+        }),
       })
-      .catch((error) => {
-        res.status(500).send(error);
-      });
+        .then((response) => {
+          let data = {};
+          if (response.status === 200) {
+            data = {
+              message: 'Subscribed',
+              status: response.status,
+            };
+          } else {
+            data = {
+              message: 'Something went wrong',
+              status: response.status,
+            };
+          }
+          res.status(response.status).send(data);
+        })
+        .catch((error) => {
+          res.status(500).send({ message: 'An error has occured, please try again', status: 500, ...error });
+        });
+    } else {
+      res.status(405).send({ message: 'Only POST requests allowed', status: 405 });
+    }
   } else {
-    res.status(405).send({ message: 'Only POST requests allowed' });
+    res.status(404).send({
+      message: 'access key or api url missing',
+      status: 404,
+    });
   }
 }
